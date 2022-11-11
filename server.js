c65c24e742fe1eccd439a17f5d9ae933def05fc0
Adele <adele.souza@hotmail.com>
2022-11-10 21:36:25 -0300

    first commit server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.listen(4000, function (erro) {
+  if (erro) {
+    console.log('Erro ao iniciar')
+  } else {
+    console.log('Servidor iniciado')
+  }
+})
+
+app.get('/', function (req, res) {
+  res.end(fs.readFileSync('paginaInicial.html'))
+})
